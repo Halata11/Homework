0fbbabe1bd1cca042e14d5216e88475443431f93
Kostadin Halachev <youngwithambiti00n@gmail.com>
2023-09-20 14:29:08 +0300

    Initial commit

--- course/practice.py
+++ course/practice.py
@@ -1,6 +1,23 @@
-divisor = int(input())
-boundary = int(input())
-for number in range(boundary, divisor - 1, -1):
-    if number % divisor == 0:
-        break
-print(number)
+letterC = False
+letterO = False
+letterN = False
+word = ""
+cmd = input()
+while cmd != "End":
+    if "a" <= cmd <= "z" or "A" <= cmd <= "Z":
+        if not letterC and (cmd == "c" or cmd == "C"):
+            letterC = True
+        elif not letterO and (cmd == "o" or cmd == "O"):
+            letterO = True
+        elif not letterN and (cmd == "n" or cmd == "N"):
+            letterN = True
+        else:
+            word += cmd
+        if letterC and letterO and letterN:
+            print(word, end=" ")
+            letterC = False
+            letterO = False
+            letterN = False
+            word = ""
+    cmd = input()
+
